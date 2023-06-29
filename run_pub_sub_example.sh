4f67b176d52df8fa5afe3467bd8c3fb0a6525827
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2023-06-29 23:08:00 +0200

    Use tmux in example script

--- run_pub_sub_example.sh
+++ run_pub_sub_example.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-COMMAND="cargo run"
-cargo build -u --manifest-path $PWD/message_bus/Cargo.toml
-cargo build -u --manifest-path $PWD/message_bus_client/Cargo.toml
-gnome-terminal --working-directory=$PWD/message_bus -- bash -c "$COMMAND; bash"
-gnome-terminal --working-directory=$PWD/message_bus_client  -- bash -c "$COMMAND --example subscriber_client2; bash"
-gnome-terminal --working-directory=$PWD/message_bus_client -- bash -c "$COMMAND --example publisher_client1; bash"
+CARGO_RUN_COMMAND="cargo run --release"
+CARGO_BUILD_COMMAND="cargo build --release"
+$CARGO_BUILD_COMMAND --manifest-path=$PWD/message_bus/Cargo.toml
+$CARGO_BUILD_COMMAND --manifest-path=$PWD/message_bus_client/Cargo.toml 
+
+
+tmux new-session -d\
+    "cd $PWD/message_bus && $CARGO_RUN_COMMAND; bash" \;\
+    split-window -h "cd $PWD/message_bus_client/ && $CARGO_RUN_COMMAND --example subscriber_client2; bash" \;\
+    select-pane -t 1 \;\
+    split-window -v "cd $PWD/message_bus_client/ && $CARGO_RUN_COMMAND  --example publisher_client1; bash" \;\
+    attach\;
