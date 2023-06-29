600590df251b5a6cd36ca96b8efdadb0f25c3568
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2023-06-29 22:58:28 +0200

    fix example

--- run_pub_sub_example.sh
+++ run_pub_sub_example.sh
@@ -3,5 +3,5 @@ COMMAND="cargo run"
 cargo build -u --manifest-path $PWD/message_bus/Cargo.toml
 cargo build -u --manifest-path $PWD/message_bus_client/Cargo.toml
 gnome-terminal --working-directory=$PWD/message_bus -- bash -c "$COMMAND; bash"
-gnome-terminal --working-directory=$PWD/message_bus_client -- bash -c "$COMMAND --example publisher_client1; bash"
 gnome-terminal --working-directory=$PWD/message_bus_client  -- bash -c "$COMMAND --example subscriber_client2; bash"
+gnome-terminal --working-directory=$PWD/message_bus_client -- bash -c "$COMMAND --example publisher_client1; bash"
